5885a01b6fd450393c7bcc5e2b8a9d3c0adc0931
Turazzi <analivia.turazzi@gmail.com>
2025-06-05 23:38:39 -0300

    Add home site base

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,26 @@
+import React from "react";
+import '../style/Home.css'
+import profileImage from '../assets/icons/perfil.jpeg';
 
-function Home() {
-    return null;
+
+const Home = () => {
+    return (
+        <main className='page-container'>
+        
+            <section className='section-home'>
+
+                <img
+                    src={profileImage}
+                    alt="Foto de perfil Ana Livia Turazzi"
+                    className='profile-image'
+                />
+                <h2 className='page-container'>Olá, eu sou a Ana Livia Turazzi!</h2>
+                <p className='page-tile'>Desenvolvedora FullStack</p>
+
+            </section>
+
+        </main>
+    )
 }
 
 export default Home;
